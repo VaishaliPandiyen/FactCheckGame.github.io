dd202431e9fd8b15f71f9288913e9347a39d41c7
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2023-07-24 13:01:47 +0100

    .

--- static/js/game.js
+++ static/js/game.js
@@ -1,26 +1,13 @@
-let claimCard = document.querySelector(".claim_wrapper");
-let card = document.querySelector(".card");
+let card1 = document.querySelector(".card_1");
 let sec1 = document.querySelector(".sec_1");
-
-claimCard.addEventListener("click", () => {
-  claimCard.style.display = "None";
-  card.style.transition = "width 1s ease-in-out";
-  card.style.width = "600px";
-  sec1.style.display = "flex";
-});
-
-let sec1nxt = document.querySelector(".sec_1_nxt");
 let sec2 = document.querySelector(".sec_2");
-
-sec1nxt.addEventListener("click", () => {
-  sec1.style.display = "none";
-  sec2.style.display = "flex";
-});
-
-let sec2nxt = document.querySelector(".sec_2_nxt");
 let sec3 = document.querySelector(".sec_3");
 
-sec2nxt.addEventListener("click", () => {
-  sec2.style.display = "none";
-  sec3.style.display = "flex";
+sec1.addEventListener("click", () => {
+  sec1.style.display = "None";
+  sec2.style.display = "Flex";
+  setTimeout(() => {
+    // sec2.style.display = "None";
+    sec3.style.display = "Flex";
+  }, 3000); // 30000 ms = 30 secs
 });
