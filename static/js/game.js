5a264d6a388873f3b83b9b5fe76d294c95811425
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2023-07-26 12:40:38 +0100

    clues and sources info display

--- static/js/game.js
+++ static/js/game.js
@@ -8,7 +8,7 @@ sec1.addEventListener("click", () => {
   // sec2.style.display = "Flex";
   sec3.style.display = "Flex";
   // setTimeout(() => (sec3.style.opacity = "1"), 5000);
-  sec3.style.opacity = "1"
+  sec3.style.opacity = "1";
 });
 
 let click_texts = document.querySelectorAll(".click_text");
@@ -23,6 +23,12 @@ let srcs = document.querySelector(".sources");
 
 let slant = document.querySelector(".slant_border");
 
+/* 
+
+CLICKING CLUE(S)
+
+*/
+
 let card3_click1_basics = () => {
   for (let c of click_texts) {
     c.style.position = "relative";
@@ -31,28 +37,30 @@ let card3_click1_basics = () => {
   slant.style.transition = "left 2s ease, opacity 2s ease";
   slant.style.opacity = "0";
 };
-
-// To achieve the sliding out effect, you should set the left and opacity properties first and then change the display property. 
-
+// To achieve the sliding out effect, you should set the left and opacity properties first and then change the display property.
 function handleClueCardClick(card) {
   card3_click1_basics();
-  src_card.style.display = "None"
+  src_card.style.display = "None";
   clue_title.style.transition = "left 2s ease, opacity 1s ease";
   clue_title.style.left = "-100%";
   clue_title.style.opacity = "0";
-  setTimeout(() => {
-    slant.style.display = "none"; 
-  }, 3000);
-
   // Use a small delay to apply the opacity transition after changing the display property
   setTimeout(() => {
     clues.style.display = "flex";
-    clue_title.style.display = "none"; // Set "none" in lowercase
-    clues.style.transition = "opacity 3s ease";
+  }, 1000);
+  setTimeout(() => {
+    clue_title.style.display = "none";
+    clues.style.transition = "opacity 2s ease";
     clues.style.opacity = "1";
   }, 1500);
   slant.style.left = "-10%";
-  clue_card.style.width = "760px";
+  setTimeout(() => {
+    slant.style.display = "none";
+  }, 3000);
+  // Adjust clue_card width after some time to prevent the clue_title from coming to the middle of the div before easing out on the left side
+  setTimeout(() => {
+    clue_card.style.width = "760px";
+  }, 1000);
 }
 
 /*
@@ -61,25 +69,109 @@ CLUES CAROUSEL
 
 */
 
-let currentIndex = 0;
+let currentIndex_1 = 0;
 const clue = document.querySelectorAll(".clue");
 
 function showClue(index) {
-  clue.forEach((text, i) => {
+  clue.forEach((clue, i) => {
     if (i === index) {
-      text.classList.add("active");
+      clue.classList.add("active");
     } else {
-      text.classList.remove("active");
+      clue.classList.remove("active");
     }
   });
 }
 
 function showNextClue() {
-  currentIndex = (currentIndex + 1) % texts.length;
-  showClue(currentIndex);
+  currentIndex_1 = (currentIndex_1 + 1) % clue.length;
+  showClue(currentIndex_1);
 }
 
 function showPreviousClue() {
-  currentIndex = (currentIndex - 1 + texts.length) % texts.length;
-  showClue(currentIndex);
+  currentIndex_1 = (currentIndex_1 - 1 + clue.length) % clue.length;
+  showClue(currentIndex_1);
+}
+
+/* 
+
+CLICKING SOURCE(S)
+
+*/
+
+let card3_click2_basics = () => {
+  for (let c of click_texts) {
+    c.style.position = "relative";
+    c.style.opacity = "0";
+  }
+  slant.style.transition = "left 2s ease, opacity 2s ease";
+  slant.style.opacity = "0";
+};
+// To achieve the sliding out effect, you should set the left and opacity properties first and then change the display property.
+function handleSrcCardClick(card) {
+  card3_click1_basics();
+  clue_card.style.display = "None";
+  src_title.style.transition = "left 2s ease, opacity 1s ease";
+  src_title.style.left = "-100%";
+  src_title.style.opacity = "0";
+  // Use a small delay to apply the opacity transition after changing the display property
+  setTimeout(() => {
+    srcs.style.display = "flex";
+  }, 1000);
+  setTimeout(() => {
+    src_title.style.display = "none";
+    srcs.style.transition = "opacity 2s ease";
+    srcs.style.opacity = "1";
+  }, 1500);
+  slant.style.left = "-10%";
+  setTimeout(() => {
+    slant.style.display = "none";
+  }, 3000);
+  // Adjust clue_card width after some time to prevent the clue_title from coming to the middle of the div before easing out on the left side
+  setTimeout(() => {
+    src_card.style.width = "760px";
+  }, 1000);
+}
+
+/*
+
+SOURCES CAROUSEL
+
+*/
+
+let currentIndex_2 = 0;
+const src = document.querySelectorAll(".source");
+
+function showSources(index) {
+  src.forEach((src, i) => {
+    if (i === index) {
+      src.classList.add("active");
+    } else {
+      src.classList.remove("active");
+    }
+  });
+}
+
+function showNextSource() {
+  currentIndex_2 = (currentIndex_2 + 1) % src.length;
+  showSources(currentIndex_2);
+}
+function showPreviousSource() {
+  currentIndex_2 = (currentIndex_2 - 1 + src.length) % src.length;
+  showSources(currentIndex_2);
+}
+
+/*
+
+TOGGLE BETWEEN CLUES AND SOURCES
+
+*/
+
+function viewSources() {
+  clues.style.opacity = "0";
+  srcs.style.display = "flex";
+
+  setTimeout(() => {
+    clues.style.display = "none";
+    srcs.style.opacity = "1";
+  }, 1000);
 }
