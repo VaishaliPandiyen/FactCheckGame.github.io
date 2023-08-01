044f23d933bb6a68404473f5660d2ea5b1c0b40f
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2023-08-01 22:27:40 +0100

    json & async

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,31 @@
+/* */
+
+const fetchClaims = async () => {
+  try {
+    let response = await fetch("../data/claims.json");
+    if (!response.ok) {
+      throw new Error("Error loading claims");
+    }
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching claims:", error);
+    return null;
+  }
+};
+
+// let claims;
+const getClaimsArray = async () => {
+  try {
+    let claimsObject = await fetchClaims();
+    let claimsArray = claimsObject.claims;
+    return claimsArray;
+    // console.log(claims);
+  } catch (error) {
+    return "Error:", error;
+  }
+};
+
 /* 
 
 ON GAME LOAD
@@ -12,24 +40,23 @@ let sec4 = document.querySelector(".sec_4");
 
 const gameLoad = () => {
   sec1.style.opacity = "1";
-  sec1.style.transition = "opacity 1s ease";
+  sec1.style.transition = "opacity 1.2s ease";
 
   setTimeout(() => {
     sec1.style.opacity = "0";
     setTimeout(() => {
       sec1.style.display = "None";
     }, 500);
-  }, 1000);
+  }, 1500);
   setTimeout(() => {
     sec2.style.display = "Flex";
     setTimeout(() => (sec2.style.opacity = "1"), 500);
-  }, 1500);
+  }, 2000);
   setTimeout(() => {
     sec2n3.style.top = "25.5%";
     sec3.style.display = "Flex";
     sec3.style.transform = "scale(0.8)";
     setTimeout(() => (sec3.style.opacity = "0.5"), 500);
-
     sec2.addEventListener("mouseenter", () => {
       sec2n3.style.top = "25.5%";
       sec3.style.transform = "scale(0.8)";
@@ -47,28 +74,86 @@ const gameLoad = () => {
   }, 3000);
 };
 
-gameLoad();
-
 /* ----------------------------------------------------- */
 
 /*
 
-CLICK TO SHOW CLUES AND SOURCES
+LOAD CLAIM OBJECT INFO IN GAME:
 
 */
+const c_no = document.getElementById("c_no");
+const c_who = document.getElementById("c_who");
+const c_when = document.getElementById("c_when");
+const c_what = document.getElementById("c_what");
+
+// const playBtn = document.getElementById("playBtn");
+
+// let n;
+
+let n = 0;
+
+let clueWrapper = document.querySelector(".i_container");
+
 let click_texts = document.querySelectorAll(".click_text");
 
 let card3 = document.querySelector(".card_3");
 let clue_title = card3.querySelector(".click_text");
 let clues = document.querySelector(".clues");
-
-/*  CLICKING CLUE(S) */
-
 let card3_click1_basics = () => {
   clue_title.opacity = "0";
 };
 
 let isCluesSeen = false;
+
+let currentIndex = 0;
+const viewedClues = [];
+
+const clue = document.querySelectorAll(".clue");
+
+let emoji = document.getElementById("emoji");
+let verdict = document.getElementById("verdict");
+let verdict_title = document.getElementById("verdict_title");
+let verdict_content = document.getElementById("verdict_content");
+let verdict_info = document.getElementById("verdict_info");
+let end_action = document.getElementById("end_action");
+
+const loadClaimInfo = async (n) => {
+  try {
+    const claims = await getClaimsArray();
+    if (claims && claims.length > n) {
+      c = claims[n];
+      c_no.innerHTML = `Claim ${c["id"]}`;
+      c_who.innerHTML = c["involves"];
+      c_what.innerHTML = c["background"];
+      c_when.innerHTML = c["claim"];
+      // console.log(c["clues"]);
+      let clues = c["clues"];
+      for (let eachClue in clues) {
+        let p = document.createElement("p");
+        p.setAttribute("class", "clue");
+        p.innerHTML = clues[eachClue];
+        clueWrapper.appendChild(p);
+        // if (eachClue == 0) {
+        //   p.setAttribute("class", "active");
+        // }
+      }
+      // console.log(clue);
+      gameLoad();
+    } else {
+      console.log("Claims array is empty or couldn't be fetched.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  clueWrapper.querySelectorAll(".clue")[0].classList.add("active");
+};
+
+// playBtn.addEventListener("click", () => {
+//   n = 0;
+//   loadClaimInfo();
+// });
+
+loadClaimInfo(n);
 
 const handleClueCardClick = (card) => {
   isCluesSeen = true;
@@ -89,17 +174,6 @@ const handleClueCardClick = (card) => {
     clues.style.opacity = "1";
   }, 1000);
 };
-
-/*
-
-CLUES CAROUSEL
-
-*/
-
-let currentIndex = 0;
-const viewedClues = [];
-
-const clue = document.querySelectorAll(".clue");
 
 const showClue = (index) => {
   clue.forEach((clue, i) => {
@@ -128,23 +202,8 @@ const showPreviousClue = () => {
   }
 };
 
-/* ----------------------------------------------------- */
-
-/*
-
-VERDICT
-
-*/
-
-let emoji = document.getElementById("emoji");
-let verdict = document.getElementById("verdict");
-let verdict_title = document.getElementById("verdict_title");
-let verdict_content = document.getElementById("verdict_content");
-let verdict_info = document.getElementById("verdict_info");
-let end_action = document.getElementById("end_action");
-
-let g1c2a1s;
-let g1c2a1 = () => (g1c2a1s = true);
+// let g1c2a1s;
+// let g1c2a1 = () => (g1c2a1s = true);
 
 const verdicts = {
   0: "How did you know 200 dogs showed up just because he’s a dog lover?",
@@ -153,14 +212,15 @@ const verdicts = {
   3: "A neighborhood app post by his family does sound credible. However…",
   4: "Now it is good enough to be true!",
 };
+
 // To find the maximum key (property) in the verdicts object, get the keys as an array using Object.keys() and then use reduce() to find max value instead of Math.max(), as the keys in the  object are strings
 let v_n = Object.keys(verdicts).reduce(
   (max, key) => (Number(key) > max ? Number(key) : max),
   -Infinity
 );
-for (let i = 0; i <= v_n; i++) {
-  console.log(i);
-}
+// for (let i = 0; i <= v_n; i++) {
+//   console.log(i);
+// }
 
 const showVerdict = (verdict, v) => {
   sec2n3.style.opacity = "0";
@@ -223,6 +283,21 @@ OTHERS
 
 */
 
-const more = () => setTimeout(() => (window.location.href = "fc1.html"), 1000);
-
 const exit = () => setTimeout(() => (window.location.href = "play.html"), 1000);
+
+const more = async () => {
+  n = 1;
+  try {
+    const claims = await getClaimsArray();
+    console.log(claims[n]["id"]);
+    if (n < claims.length) {
+      await showClaim(n);
+    } else {
+      exit();
+    }
+  } catch (error) {
+    console.error("Error fetching claims:", error);
+    exit();
+  }
+  setTimeout(() => (window.location.href = "game.html"), 1000);
+};
